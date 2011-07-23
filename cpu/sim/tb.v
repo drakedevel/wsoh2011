739896c830ca4904aaed20765391d5f1273fc65f
Andrew Drake <adrake@andrew.cmu.edu>
2011-07-23 06:24:18 -0700

    Remove clutter.

--- cpu/sim/tb.v
+++ cpu/sim/tb.v
@@ -33,28 +33,8 @@ module tb();
       #25;
       rst_b = 1'b1;
 
-/*      
-      wait (hatch_address == 32'h00000000) hatch_instruction = { `OP_GOTO, 8'b0, 16'h0, 16'h0007 };
-      wait (hatch_address == 32'h00000007) hatch_instruction = { `JSOP_PUSH, 8'b1, 32'h00001001 };
-      wait (hatch_address == 32'h0000000d) hatch_instruction = { `JSOP_PUSH, 8'b1, 32'h1337D00D };
-      wait (hatch_address == 32'h00000013) hatch_instruction = { `JSOP_CALL, 8'b1, 32'h00000001 };
-      wait (hatch_address == 32'h00000019) hatch_instruction = { `JSOP_NOP, 8'b0, 32'h0 };
-
-      wait (hatch_address == 32'h00001001) hatch_instruction = { `JSOP_PUSH, 8'b1, 32'h15410AAA };
- */
-      wait (hatch_address == 32'h00001007) hatch_instruction = { `JSOP_RETURN, 8'b0, 32'h0 };
-      wait (hatch_address == 32'h00000019) hatch_instruction = { `JSOP_NOP, 8'b0, 32'h0 };
-
       #1000;
-/*
-      wait (hatch_address == 32'h0000101F) hatch_instruction = { `JSOP_NOP, 8'b0, 32'h0 };
 
-      wait (hatch_address == 32'h00000013) hatch_instruction = { `JSOP_NOP, 8'b0, 32'h0 };
-*/
-/*
-      wait (hatch_address == 32'h00000018) hatch_instruction = { `OP_BITAND, 8'b0, 32'h0 };
-      wait (hatch_address == 32'h0000001e) hatch_instruction = { `OP_NOP, 8'b0, 32'b0 };
-*/
       $finish();
    end
 
